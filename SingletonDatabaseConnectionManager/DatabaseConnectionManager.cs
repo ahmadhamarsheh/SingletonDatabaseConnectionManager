0436d0634e0513148ed5ee0cc09c3a9d06b1d522
AhmadHamarsheh <ahmadhamarshe@outlook.com>
2024-07-23 02:12:54 +0300

    Add project files.

---
+++ SingletonDatabaseConnectionManager/DatabaseConnectionManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SingletonDatabaseConnectionManager
+{
+    public class DatabaseConnectionManager
+    {
+        private static DatabaseConnectionManager _instance;
+
+        private static readonly object _lock = new object();
+
+        private DatabaseConnectionManager()
+        {
+            Console.WriteLine("DatabaseConnectionManager initialized.");
+        }
+
+        public static DatabaseConnectionManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new DatabaseConnectionManager();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public void Connect()
+        {
+            Console.WriteLine("Simulated database connection opened.");
+        }
+
+        public void Disconnect()
+        {
+            Console.WriteLine("Simulated database connection closed.");
+        }
+    }
+}
---
+++ SingletonDatabaseConnectionManager/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+
+namespace SingletonDatabaseConnectionManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbManager1 = DatabaseConnectionManager.Instance;
+            var dbManager2 = DatabaseConnectionManager.Instance;
+
+            if (dbManager1 == dbManager2)
+            {
+                Console.WriteLine("Both instances are the same.");
+            }
+            else
+            {
+                Console.WriteLine("Instances are different.");
+            }
+
+            dbManager1.Connect();
+            dbManager2.Disconnect();
+        }
+    }
+}
